fc06b2258f85784ca2385f1ca884647e933ba6fa
baharudinmulya <baharu12185173@bsi.ac.id>
2021-09-26 07:54:12 +0700

    first

---
+++ Assets/Scripts/GameFlowManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameFlowManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static GameFlowManager _instance;
+
+    public static GameFlowManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameFlowManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("GameFlowManager Not Found!");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    [Header("UI")]
+    public UIGameOver GameOverUI;
+
+    #endregion
+
+    private bool _isGameOver;
+    
+    public bool IsGameOver => _isGameOver;
+
+    private void Start()
+    {
+        _isGameOver = false;
+    }
+
+    public void GameOver()
+    {
+        _isGameOver = true;
+        if (ScoreManager.Instance.CurrentScore > ScoreManager.Instance.HighScore)
+        {
+            ScoreManager.Instance.SetHighScore();
+        }
+        
+        GameOverUI.Show();
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Match Three
+Match Three Puzzle game made as part of Agate Studi Independen Game Programming Course
+
+## How to play
+
+### Build
+You can play the game by accessing the build version on the [releases page](https://github.com/NaufalA/Match-Three/releases)
+
+## Stuff changed or added
